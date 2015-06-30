94fd30310314cae35da0a0b12984d36c2725f53f
Adam Collado <adam.collado@gmail.com>
2015-06-29 21:52:47 -0400

    Now using Argparser
    Should make it easier for parsing command line options introduced in
    future versions. Also made it easier to perform tests by separating key
    functionaility into testable functions.

--- logfind/main.py
+++ logfind/main.py
@@ -4,14 +4,17 @@ import sys
 import os
 import re
 import fileinput
+import argparse
 
 
-def search_files(log_files, regex_expression):
+def search_files(log_files, arguments):
+    parser = return_parser()
+    parsed_arguments = parser.parse_args(arguments)
     files = []
 
     with fileinput.input(files=log_files) as f:
         for line in f:
-            if re.search(regex_expression, line):
+            if search_method(parsed_arguments, line):
                 # print("{0} in file {1}".format(line, fileinput.filename()))
                 # print(fileinput.filename())
                 files.append(fileinput.filename())
@@ -19,7 +22,23 @@ def search_files(log_files, regex_expression):
     return files
 
 
-if __name__ == '__main__':
+def search_method(parsed_arguments, line):
+    if parsed_arguments.o:
+        regex = "(\\b" + "\\b|\\b".join(parsed_arguments.search_words) + "\\b)"
+    else:
+        # NOTE: This is a long operation. Perhaps use any and all instead?
+        regex = "(?=.*\\b" + "\\b)(?=.*\\b".join(parsed_arguments.search_words) + "\\b)"
+    return re.search(regex, line)
+
+
+def return_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", help="perform an OR search", action="store_true")
+    parser.add_argument("search_words", help="words that will be searched for", nargs='+')
+    return parser
+
+
+def main(arguments):
     # TODO: Make a configuration file to handle file names and directory information
     user_directory = os.path.expanduser('~')
     logfind_file = os.path.join(user_directory, '.logfind')
@@ -28,18 +47,15 @@ if __name__ == '__main__':
         # TODO: Don't include empty lines into the list
         log_files = f.read().splitlines()
 
-    # print(lines)
-    # TODO: Convert to Try/Except
     if not os.path.isfile(logfind_file):
         sys.exit("No .logfind file exists. Please create one.")
 
     # TODO: Check if the files in the .logfind file exist
-    search_words = sys.argv[1:]
-    if not search_words:
-        sys.exit("No arguments were entered. Please enter arguments.")
 
-    # print(search_words)
-    regex_expression = "(" + ")|(".join(search_words) + ")"
-
-    files_found = search_files(log_files, regex_expression)
+    files_found = search_files(log_files, arguments)
     print('\n'.join('{}'.format(f) for f in files_found))
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
