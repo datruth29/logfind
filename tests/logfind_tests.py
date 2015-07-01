6ec0b0804c5af39da7b407436cd6f18deffca0ec
Adam Collado <adam.collado@gmail.com>
2015-06-30 21:51:24 -0400

    Added test cases

--- tests/logfind_tests.py
+++ tests/logfind_tests.py
@@ -1,14 +1,44 @@
-from nose.tools import *
-import logfind
+from nose.tools import assert_raises, assert_list_equal, assert_true, assert_false
+from logfind import logfind
+import os
 
 
-def setup():
-    print("SETUP!")
+class TestFindlog(object):
+    def setup(self):
+        self.log_files = [os.path.join(os.path.dirname(__file__), 'data', 'test_logfile')]
+        self.arguments_or = ['-o', 'this', 'is', 'a', 'test']
+        self.arguments_and = ['this', 'is', 'a', 'test']
 
+    def test_return_parsar(self):
+        parser = logfind.return_parser()
+        with assert_raises(SystemExit):
+            parser.parse_args()
+            parser.parse_args(['-o'])
 
-def teardown():
-    print("TEAR DOWN!")
+    def test_search_files_using_or(self):
+        files_found = logfind.search_files(self.log_files, self.arguments_or)
+        assert_list_equal(files_found, self.log_files)
 
+    def test_search_files_using_and(self):
+        files_found = logfind.search_files(self.log_files, self.arguments_and)
+        assert_list_equal(files_found, self.log_files)
 
-def test_basic():
-    print("I RAN!")
+    def test_search_method_using_or(self):
+        parser = logfind.return_parser()
+        parsed_arguments = parser.parse_args(self.arguments_or)
+        assert_true(logfind.search_method(parsed_arguments, 'this is a test'))
+
+    def test_search_method_using_or_fail(self):
+        parser = logfind.return_parser()
+        parsed_arguments = parser.parse_args(self.arguments_or)
+        assert_false(logfind.search_method(parsed_arguments, 'blah blah blah'))
+
+    def test_search_method_using_and(self):
+        parser = logfind.return_parser()
+        parsed_arguments = parser.parse_args(self.arguments_and)
+        assert_true(logfind.search_method(parsed_arguments, 'this is a test'))
+
+    def test_search_method_using_and_fail(self):
+        parser = logfind.return_parser()
+        parsed_arguments = parser.parse_args(self.arguments_and)
+        assert_false(logfind.search_method(parsed_arguments, 'this is a '))
